c604069e7cd602eda2080e352ce69dffab3d491a
claudia <clou.7loymed@gmail.com>
2017-03-14 15:36:25 -0500

    historial

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(init);
 
 var currentSection = null;
+var currentGameID;
 
 function init()
 {
@@ -8,8 +9,18 @@ function init()
 	$('#btn-saludo').click(onClickBtnSaludo);
 	$('#btn-nombres').click(onClickBtnNombre);
     $('#btn-historial').click(onClickBtnHistorial);
+	$('#btn-comentar').click(onClickBtnComentar);
+	$('#lista-juegos').on('click', 'button', onClickItemJuego);
 
 	TweenMax.from($('#saludo h1'), 1, {marginBottom:'0px', ease:Elastic.easeOut});
+}
+
+function onClickItemJuego(){
+	var idGame = $(this).parent().data('idgame');
+	console.log(idGame);
+	gotoSection('historial-detalle');
+	getComentarios(idGame);
+	currentGameID = idGame;
 }
 
 function onClickBtnSaludo() {
@@ -45,7 +56,7 @@ function onClickBtnNombre() {
 }*/
 
 function onClickBtnHistorial(evt){
-	//evt.preventDefault();
+	evt.preventDefault();
     gotoSection('historial');
 	getHistorial();
 }
@@ -61,17 +72,59 @@ function getHistorial(){
 	});
 }
 
+function onClickBtnComentar(){
+	enviarComentario(currentGameID, $('#name').val(), $('#content').val());
+}
+
+function enviarComentario(_idGame, _name, _content){
+	$.ajax({
+		url: 'http://test-ta.herokuapp.com/games/'+_idGame+'/comments',
+		type: 'POST',
+		data:{comment:{name:_name, content:_content, game_id:_idGame}}
+	}).done(function(_data){
+		console.log(_data);
+		getComentarios(_idGame);
+	});
+}
+
+function getSingleGame(_idGame){
+	$.ajax({
+		url:"http://test-ta.herokuapp.com/games/"+_idGame
+	}).done(function(_data){
+		console.log(_data);
+	})
+}
+
+function getComentarios(_idGame){
+	$.ajax({
+		url:"http://test-ta.herokuapp.com/games/"+_idGame+"/comments"
+	}).done(function(_data){
+		console.log(_data);
+		dibujarComentarios(_data);
+	})
+}
+
 function dibujarHistorial(_datos){
 	var lista = $('#lista-juegos');
 	
 	for (var i in _datos){
 		console.log(_datos[i].winner_player);
-		var html = '<li class="list-group-item">WINNER:'+_datos[i].winner_player+'</li>';
+		
+		var html = '<li data-idgame="'+ _datos[i].id +'" class="list-group-item">'+_datos[i].winner_player+'le gano a  ' +_datos[i].loser_player + ' en '+ _datos[i].number_of_turns_to_win + ' movimientos <button class="btn-info">comentar</button></li>';
 		lista.append(html);
 	}
 }
 
-
+function dibujarComentarios(_datos){
+	var lista = $('#lista-comentarios');
+	
+	lista.empty();
+	
+	for (var i in _datos){
+		var html = '<li class="list-group-item">'+_datos[i].name+' dice: <p>'+_datos[i].content+'</p> </li>';
+		lista.append(html);
+	}
+}
 //MAYUSCULA
 /*function convertirMayus(texto){
     
